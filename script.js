5fcbd5c0ab4383d4eed29f967fa07f05af1b5246
Vanessa Chen <vanchen07@gmail.com>
2020-05-21 19:21:24 -0700

    add images

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ const textarea = document.getElementById('text');
 const readBtn = document.getElementById('read');
 const toggleBtn = document.getElementById('toggle');
 const closeBtn = document.getElementById('close');
+
+const data = [
+    {
+        image: './img/drink.jpg',
+        text: "I'm Thirsty",
+    }
+];
